fab3b39afa4cb83c2cdcef080ac4e6485876ba35
Astha <jainastha587@gmail.com>
2026-01-29 20:11:16 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.on('error',(error)=>{
+        console.log("Error: ",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at PORT ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed !!! ",error)
+})
 
 
 
