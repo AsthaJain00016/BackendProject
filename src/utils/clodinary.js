830270b9fc6ec42efb264aea7a19d9872b53d8fd
Astha <jainastha587@gmail.com>
2026-01-31 18:54:51 +0530

    Register User

--- src/utils/clodinary.js
+++ src/utils/clodinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary=async(localFilePath)=>{
             resource_type:"auto"
         })
         // file has been uploaded successfully 
-        console.log(`File has been uploaded successfully on cloudinary`,response.url);
+        // console.log(`File has been uploaded successfully on cloudinary`,response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     }catch(error){
         fs.unlinkSync(localFilePath)  // remove locally saved temporary files as the upload operation failed
