a6001a2ae12d41912d9c7b618d2405dcd29747c6
Astha <jainastha587@gmail.com>
2026-02-01 15:42:48 +0530

    login logout features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../contollers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../contollers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router()
 
 router.route("/register").post(upload.fields([
@@ -13,5 +14,12 @@ router.route("/register").post(upload.fields([
         maxCount:1
     }
 ]),registerUser)
+
+router.route("/login").post(loginUser)
+
+// Secured Routes
+router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 
 export default router;
